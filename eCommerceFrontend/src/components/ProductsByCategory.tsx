9f5e7049ed89047e004fe05ae5c50af434617ee3
g4stlyx <g4stlyonline@gmail.com>
2024-08-21 12:33:20 +0300

    admin categories page

--- eCommerceFrontend/src/components/ProductsByCategory.tsx
+++ eCommerceFrontend/src/components/ProductsByCategory.tsx
@@ -1,13 +1,62 @@
-//! "/categories/:categoryId/products" or "/categories/:categoryId" : to show products for a specific category.
-
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Product } from '../types/types'; 
+import { searchAndFilterProductsApi } from './api/ProductApiService';
+import { Card, Col } from 'react-bootstrap';
 
 const ProductsByCategory: React.FC = () => {
+  const { category } = useParams<{ category: string }>(); 
+  const [products, setProducts] = useState<Product[]>([]); 
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await searchAndFilterProductsApi({ category });
+        setProducts(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError('Failed to fetch products: '+ err);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, [category]);
+
+  if (loading) return <div>Loading...</div>; 
+  if (error) return <div>{error}</div>;
+
   return (
     <div>
-      
+      <h2>Products in {category}</h2>
+      <div className="products-grid">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <Col key={product.id}>
+              <a href={`/categories/${product.category.name}/products/${product.id}`} className="text-decoration-none">
+                <Card className="h-100">
+                  <Card.Img
+                    variant="top"
+                    src={product.imgSrc}
+                    alt={product.name}
+                    style={{ height: "200px", objectFit: "cover" }}
+                  />
+                  <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>{product.price} $</Card.Text>
+                  </Card.Body>
+                </Card>
+              </a>
+            </Col>
+          ))) : (
+          <div>No products found in this category.</div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsByCategory
+export default ProductsByCategory;
--- eCommerceFrontend/src/components/admin_pages/AdminCategoryUpdate.tsx
+++ eCommerceFrontend/src/components/admin_pages/AdminCategoryUpdate.tsx
@@ -1,13 +1,96 @@
-//! /administrator/categories/:categoryId/edit, create or update a category.
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getAllCategoriesApi,
+  deleteCategoryApi,
+} from "../api/CategoryApiService";
+import { Button, Table } from "react-bootstrap";
+import { Category } from "../../types/types";
 
-import React from 'react'
+const AdminCategories: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const navigate = useNavigate();
 
-const AdminCategoryUpdate: React.FC = () => {
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
+  const getAllCategories = async () => {
+    try {
+      const response = await getAllCategoriesApi();
+      setCategories(response.data);
+    } catch (error) {
+      console.error("Error fetching categories", error);
+    }
+  };
+
+  const deleteCategory = async (categoryId: number) => {
+    const confirmDelete = window.confirm("Emin Misiniz?");
+    if (confirmDelete) {
+      try {
+        await deleteCategoryApi(categoryId);
+        setCategories(
+          categories.filter((category) => category.id !== categoryId)
+        );
+      } catch (error) {
+        console.error("Error deleting category", error);
+      }
+    }
+  };
+
+
+  const handleAddCategory = () => {
+    navigate("/administrator/categories/-1/edit");
+  };
+
+  const handleEditCategory = (categoryId: number) => {
+    navigate(`/administrator/categories/${categoryId}/edit`);
+  };
+
   return (
-    <div>
-      
+    <div className="admin-categories-container">
+      <h2>Kategorileri Yönet</h2>
+      <Button variant="primary" onClick={handleAddCategory} className="mb-3">
+        Ekle
+      </Button>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>İsim</th>
+            <th>Açıklama</th>
+            <th>Eylemler</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr key={category.id}>
+              <td>{category.id}</td>
+              <td>
+                <a href={`/categories/${category.name}`} style={{textDecoration:"none"}}>{category.name}</a>
+              </td>
+              <td>{category.description}</td>
+              <td>
+                <Button
+                  variant="warning"
+                  onClick={() => handleEditCategory(category.id)}
+                  className="me-2"
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={() => deleteCategory(category.id)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default AdminCategoryUpdate
+export default AdminCategories;
